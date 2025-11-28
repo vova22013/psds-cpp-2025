77d65d3e006727f7de31d3e9b07db4758b709d33
vova22013 <104551154+vova22013@users.noreply.github.com>
2025-11-28 16:45:45 +0000

    Update print_bits.cpp

--- 01_week/tasks/print_bits/print_bits.cpp
+++ 01_week/tasks/print_bits/print_bits.cpp
@@ -1,7 +1,25 @@
-#include <cstddef>
-#include <stdexcept>
-
+#include <iostream>
+#include <algorithm>
 
 void PrintBits(long long value, size_t bytes) {
-    throw std::runtime_error{"Not implemented"};
+    std::string str_bset = "";
+    
+    auto bits = bytes * 8;
+
+    for (size_t i = 0; i < bits; ++i) {
+        auto b_i = (value >> i) & 1u;
+
+        if (static_cast<bool>(b_i)) str_bset += "1";
+        else str_bset += "0";
+
+        if (i % 4 == 3 && i != bits - 1) {
+            str_bset += '\'';
+        }
+    }
+
+    str_bset += "b0";
+    
+    std::reverse(str_bset.begin(), str_bset.end());
+    
+    std::cout << str_bset << "\n";
 }
