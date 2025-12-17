a2130ad4e72714f61d4e85423fe25f583c977bd1
vova_shtin@mail.ru <vova_shtin@mail.ru>
2025-12-17 21:26:06 +0500

    add (solution): add find_all task

--- 03_week/tasks/find_all/find_all.cpp
+++ 03_week/tasks/find_all/find_all.cpp
@@ -1,6 +1,25 @@
 #include <stdexcept>
+#include <vector>
 
+std::vector<size_t> FindAll(const std::vector<int>& vec, bool (*func)(int)) {
+    if (vec.size() == 0 || func == nullptr) return std::vector<size_t>(0);
 
-/* return_type */ FindAll(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<size_t> res(vec.size() / 2);
+    size_t res_ind = 0;
+    
+    for(size_t i = 0; i < vec.size(); ++i) {
+        if (res.size() == res_ind) 
+            res.resize(res_ind * 2);
+        
+        if (func(vec[i])) {
+            if (res.empty()) res.push_back(i);
+            else res[res_ind] = i;
+            ++res_ind; 
+        }
+    }
+    
+    res.erase(res.begin() + res_ind, res.end());
+    res.shrink_to_fit();
+    
+    return res;
 }
