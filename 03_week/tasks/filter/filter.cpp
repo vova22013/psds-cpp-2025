cd158cf68f3740b2c60afb030f651e4699bb1573
vova_shtin@mail.ru <vova_shtin@mail.ru>
2025-12-17 21:25:50 +0500

    add (solution): add filter task

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,17 @@
 #include <stdexcept>
+#include <vector>
 
+void Filter(std::vector<int>& vec, bool (*func)(int)) {
+    if (vec.size() == 0 || func == nullptr) return;
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    auto last_correct_it = vec.begin();
+    for(auto it = vec.begin(); it != vec.end(); ++it) {
+        if (func(*it)) {
+            auto temp = *it;
+            *it = *last_correct_it;
+            *last_correct_it = temp;
+            ++last_correct_it;
+        }
+    }
+    vec.erase(last_correct_it, vec.end());
 }
