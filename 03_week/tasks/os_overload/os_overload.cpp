8de38a3ed9ee8e0baee720560438869e00e3b59f
vova_shtin@mail.ru <vova_shtin@mail.ru>
2025-12-17 21:26:54 +0500

    add (solution): add os_overload task

--- 03_week/tasks/os_overload/os_overload.cpp
+++ 03_week/tasks/os_overload/os_overload.cpp
@@ -1,21 +1,53 @@
 #include <stdexcept>
 #include <vector>
 #include <utility>
-
+#include <iostream>
+#include <string>
 
 struct Coord2D {
-    int x;
-    int y;
+    int x = 0;
+    int y = 0;
 };
 
 struct Circle {
     Coord2D coord;
-    unsigned radius;
+    unsigned radius = 1;
 };
 
 using CircleRegion = std::pair<Circle, bool>;
 using CircleRegionList = std::vector<CircleRegion>;
 
-/* return_type */ operator<<(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::ostream& operator<<(std::ostream& os, const Coord2D& coords) {
+    os << "(" + std::to_string(coords.x) + ", " + std::to_string(coords.y) + ")";
+    return os;    
 }
+
+std::ostream& operator<<(std::ostream& os, const Circle& circle) {
+    if (circle.radius == 0) os << "circle[]";
+    else {
+        os << "circle[" << circle.coord << ", " 
+            << "r = " << std::to_string(circle.radius) << "]";
+    }
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CircleRegion& creg) {
+    if (creg.second) os << "+";
+    else os << "-";
+    os << creg.first;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CircleRegionList& list) {
+    if (list.size() == 0) os << "{}";
+    else {
+        os << "{";
+        for(size_t i = 0; i < list.size(); ++i) {
+            os << "\n\t" << list[i];
+            if (i != list.size() - 1) os << ",";
+        }
+        os << "\n}";
+    }
+    return os;
+}
+
