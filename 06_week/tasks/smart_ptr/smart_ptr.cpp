a461e5a676c4dfe156a4eaa1924a039f746f76b7
vova_shtin@mail.ru <vova_shtin@mail.ru>
2026-02-24 01:10:19 +0500

    Solution Smart_ptr

--- 06_week/tasks/smart_ptr/smart_ptr.cpp
+++ 06_week/tasks/smart_ptr/smart_ptr.cpp
@@ -1,15 +1,14 @@
 #include <string>
+#include <iostream>
 
 class SharedPtr;
 class WeakPtr;
 
 struct Control {
-    size_t count_owners_ = 1;
+    size_t count_owners_ = 0;
     size_t count_observers_ = 0;
 
     Control();
-    Control(size_t count_owners);
-    Control(size_t count_owners, size_t count_observers);
 };
 
 class SharedPtr {
@@ -20,20 +19,22 @@ public:
     SharedPtr(const SharedPtr& other);
     SharedPtr(SharedPtr&& other) noexcept;
     ~SharedPtr();
-    
+
     SharedPtr& operator=(const SharedPtr& other);
     SharedPtr& operator=(SharedPtr&& other) noexcept;
     std::string& operator*() const;
     std::string* operator->() const;
     operator bool() const noexcept;
-    
+
     std::string* Get() const noexcept;
     Control* GetBlock() const noexcept;
     void Reset(std::string* str_ptr) noexcept;
     void Swap(SharedPtr& other) noexcept;
     size_t UseCount() const noexcept;
-    
+
 private:
+    void FreeMemory() noexcept;
+
     std::string* data_ = nullptr;
     Control* block_ = nullptr;
 };
@@ -45,21 +46,23 @@ public:
     WeakPtr(const WeakPtr& other);
     WeakPtr(WeakPtr&& other) noexcept;
     ~WeakPtr();
-    
+
     WeakPtr& operator=(const WeakPtr& other);
     WeakPtr& operator=(WeakPtr&& other) noexcept;
     const std::string& operator*() const;
     const std::string* operator->() const;
-    
+
     void Reset() noexcept;
     void Swap(WeakPtr& other) noexcept;
     size_t UseCount() const noexcept;
     bool Expired() const noexcept;
-    SharedPtr Lock() noexcept;
+    SharedPtr Lock() const noexcept;
 
     friend SharedPtr::SharedPtr(const WeakPtr& weak_ptr);
-    
+
 private:
+    void FreeBlock() noexcept;
+
     const std::string* data_ = nullptr;
     Control* block_ = nullptr;
 };
@@ -68,33 +71,31 @@ private:
 
 Control::Control() = default;
 
-Control::Control(size_t count_owners, size_t count_observers) :
-    count_owners_(count_owners), count_observers_(count_observers) {}
-
-Control::Control(size_t count_owners) : Control(count_owners, 0) {}
-
 // SharePtr's ctors and methods
 
-SharedPtr::SharedPtr() : 
-    block_(new Control()){};
+SharedPtr::SharedPtr() = default;
 
 SharedPtr::SharedPtr(std::string* str_ptr) :
     data_(str_ptr),
-    block_(new Control()){};
+    block_(new Control()) {
+    if (data_ && block_) ++block_->count_owners_;
+};
 
-SharedPtr::SharedPtr(const WeakPtr& weak_ptr) :
-    data_(const_cast<std::string*>(weak_ptr.data_)),
-    block_(weak_ptr.block_) {
-    ++block_->count_owners_;
+SharedPtr::SharedPtr(const WeakPtr& weak_ptr) {
+    if (!weak_ptr.Expired()) {
+        data_ = const_cast<std::string*>(weak_ptr.data_);
+        block_ = weak_ptr.block_;
+        if (data_ && block_) ++block_->count_owners_;
+    }
 }
 
 SharedPtr::SharedPtr(const SharedPtr& other) :
     data_(other.data_),
     block_(other.block_) {
-    ++block_->count_owners_;
+    if (data_ && block_) ++block_->count_owners_;
 }
 
-SharedPtr::SharedPtr(SharedPtr && other) noexcept :
+SharedPtr::SharedPtr(SharedPtr&& other) noexcept :
     data_(other.data_),
     block_(other.block_) {
     other.data_ = nullptr;
@@ -102,37 +103,37 @@ SharedPtr::SharedPtr(SharedPtr && other) noexcept :
 }
 
 SharedPtr::~SharedPtr() {
-    if (data_ && --block_->count_owners_ == 0) {
+    FreeMemory();
+}
+
+void SharedPtr::FreeMemory() noexcept {
+    if (block_ && ((!data_ && block_->count_owners_ == 0) || --block_->count_owners_ == 0)) {
         delete data_;
         data_ = nullptr;
-        delete block_;
-        block_ = nullptr;
+        if (block_->count_observers_ == 0) {
+            delete block_;
+            block_ = nullptr;
+        }
     }
 }
 
 SharedPtr& SharedPtr::operator=(const SharedPtr& other) {
     if (this != &other) {
-        if (data_ && --block_->count_owners_ == 0) {
-            delete data_;
-            delete block_;
-        }
+        FreeMemory();
         data_ = other.data_;
         block_ = other.block_;
-        ++block_->count_owners_;
+        if (data_ && block_) ++block_->count_owners_;
     }
     return *this;
 }
 
 SharedPtr& SharedPtr::operator=(SharedPtr&& other) noexcept {
     if (this != &other) {
-        if (data_ && --block_->count_owners_ == 0) {
-            delete data_;
-            delete block_;
-        }
+        FreeMemory();
         data_ = other.data_;
         block_ = other.block_;
         other.data_ = nullptr;
-        other.block_ = nullptr;   
+        other.block_ = nullptr;
     }
     return *this;
 }
@@ -154,13 +155,8 @@ Control* SharedPtr::GetBlock() const noexcept {
 }
 
 void SharedPtr::Reset(std::string* str_ptr = nullptr) noexcept {
-    if (block_ && --block_->count_owners_ == 0) {
-        delete data_;
-        ++block_->count_owners_;
-    } 
-    else block_ = new Control();
-    data_ = str_ptr;
-} 
+    SharedPtr(str_ptr).Swap(*this);
+}
 
 void SharedPtr::Swap(SharedPtr& other) noexcept {
     std::swap(data_, other.data_);
@@ -168,15 +164,19 @@ void SharedPtr::Swap(SharedPtr& other) noexcept {
 }
 
 size_t SharedPtr::UseCount() const noexcept {
-    return data_ ? block_->count_owners_ : 0;
+    return block_ ? block_->count_owners_ : 0;
 }
 
 SharedPtr::operator bool() const noexcept {
-    return static_cast<bool>(data_);
+    return data_ != nullptr;
 }
 
 SharedPtr MakeShared(const std::string& str) noexcept {
     return SharedPtr(new std::string(str));
+}
+
+SharedPtr MakeShared(std::string&& str) noexcept {
+    return SharedPtr(new std::string(std::move(str)));
 }
 
 SharedPtr MakeShared(const SharedPtr& other) noexcept {
@@ -198,13 +198,13 @@ WeakPtr::WeakPtr() = default;
 WeakPtr::WeakPtr(const SharedPtr& shared_ptr) :
     data_(shared_ptr.Get()),
     block_(shared_ptr.GetBlock()) {
-    ++block_->count_observers_;
+    if (block_) ++block_->count_observers_;
 }
 
 WeakPtr::WeakPtr(const WeakPtr& other) :
     data_(other.data_),
     block_(other.block_) {
-    ++block_->count_observers_;
+    if (block_) ++block_->count_observers_;
 }
 
 WeakPtr::WeakPtr(WeakPtr&& other) noexcept :
@@ -214,22 +214,31 @@ WeakPtr::WeakPtr(WeakPtr&& other) noexcept :
     other.block_ = nullptr;
 }
 
-WeakPtr::~WeakPtr() = default;
+WeakPtr::~WeakPtr() {
+    FreeBlock();
+};
+
+void WeakPtr::FreeBlock() noexcept {
+    if (block_ && (block_->count_observers_ == 0 || --block_->count_observers_ == 0) && block_->count_owners_ == 0) {
+        delete block_;
+        block_ = nullptr;
+    }
+}
 
 WeakPtr& WeakPtr::operator=(const WeakPtr& other) {
     if (this != &other) {
+        FreeBlock();
         data_ = other.data_;
-        --block_->count_observers_;
         block_ = other.block_;
-        ++block_->count_observers_;
+        if (block_) ++block_->count_observers_;
     }
     return *this;
 }
 
 WeakPtr& WeakPtr::operator=(WeakPtr&& other) noexcept {
     if (this != &other) {
+        FreeBlock();
         data_ = other.data_;
-        if (block_) --block_->count_observers_;
         block_ = other.block_;
         other.data_ = nullptr;
         other.block_ = nullptr;
@@ -246,13 +255,13 @@ const std::string* WeakPtr::operator->() const {
 }
 
 void WeakPtr::Reset() noexcept {
-    data_ = nullptr;
-    if (block_) --block_->count_observers_;
-    block_ = nullptr;
+    FreeBlock();
+    WeakPtr().Swap(*this);
 }
 
 void WeakPtr::Swap(WeakPtr& other) noexcept {
     std::swap(data_, other.data_);
+    std::swap(block_, other.block_);
 }
 
 size_t WeakPtr::UseCount() const noexcept {
@@ -263,23 +272,10 @@ bool WeakPtr::Expired() const noexcept {
     return UseCount() == 0;
 }
 
-SharedPtr WeakPtr::Lock() noexcept {
-    return UseCount() != 0 ? SharedPtr(*this) : SharedPtr();
+SharedPtr WeakPtr::Lock() const noexcept {
+    return SharedPtr(*this);
 }
 
 void Swap(WeakPtr& lft, WeakPtr& rht) noexcept {
     std::swap(lft, rht);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
